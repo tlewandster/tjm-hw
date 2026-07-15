3e70a7787e7e608db471692a901180ee2e85ccca
Tomasz Lewandowski <tlewandster@gmail.com>
2026-07-15 19:57:14 +0200

    Add a method to handle sub-task 1

--- src/pl/tlewandster/task1/Main.java
+++ src/pl/tlewandster/task1/Main.java
@@ -1,10 +1,28 @@
 package pl.tlewandster.task1;
 
+import java.util.Scanner;
+
 public class Main {
     static void main() {
         final String SET_GREEN_FONT = "\u001B[32m";
         final String RESET_FONT_COLOR = "\u001B[0m";
 
+        System.out.println(SET_GREEN_FONT + "1. Wiek i pełnoletność" + RESET_FONT_COLOR);
+        verifyAge();
+        }
+
+    private static void verifyAge() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj wiek użytkownika: ");
+        int age = scanner.nextInt();
+        if (age >= 18) {
+            System.out.println("Jesteś pełnoletni");
+        } else {
+            System.out.println("Nie jesteś pełnoletni");
+        }
+    }
 
     }
+
+
 }
