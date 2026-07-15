4ca5e61012c70a88725ea3a9e34f44d0a8639cce
Tomasz Lewandowski <tlewandster@gmail.com>
2026-07-15 18:59:50 +0200

    Add main method with terminal text color constants

---
+++ src/pl/tlewandster/task1/Main.java
@@ -0,0 +1,10 @@
+package pl.tlewandster.task1;
+
+public class Main {
+    static void main() {
+        final String SET_GREEN_FONT = "\u001B[32m";
+        final String RESET_FONT_COLOR = "\u001B[0m";
+
+
+    }
+}
