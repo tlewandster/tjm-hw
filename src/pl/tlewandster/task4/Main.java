f7adf532b06738389d94b2e09e21bc84a6871d6d
Tomasz Lewandowski <tlewandster@gmail.com>
2026-07-22 18:38:36 +0200

    feat: Add a method to handle sub-task 1

--- src/pl/tlewandster/task4/Main.java
+++ src/pl/tlewandster/task4/Main.java
@@ -2,6 +2,14 @@ package pl.tlewandster.task4;
 
 public class Main {
     static void main() {
+        // 1. Proste powitanie
+        welcome("Krzysiu");
+        welcome("Rysiu");
+        welcome("Zdzisiu");
 
+
+    }
+    static void welcome(String name){
+        System.out.printf("Welcome, %s!%n", name);
     }
 }
