943524e4c1189cfcb977d450ad1cd3d79c026f00
Tomasz Lewandowski <tlewandster@gmail.com>
2026-07-22 18:58:58 +0200

    feat: Add a method to handle sub-task 2

--- src/pl/tlewandster/task4/Main.java
+++ src/pl/tlewandster/task4/Main.java
@@ -1,5 +1,7 @@
 package pl.tlewandster.task4;
 
+@SuppressWarnings("JavaPrintToLogpoint")
+
 public class Main {
     static void main() {
         // 1. Proste powitanie
@@ -7,9 +9,19 @@ public class Main {
         welcome("Rysiu");
         welcome("Zdzisiu");
 
+        // 2. Dodawanie liczb
+        int firstNumberToBeMultiplied = 3;
+        int secondNumberToBeMultiplied = 5;
+        System.out.printf("%d * %d = %d%n", firstNumberToBeMultiplied, secondNumberToBeMultiplied, mult(firstNumberToBeMultiplied, secondNumberToBeMultiplied));
+
 
     }
-    static void welcome(String name){
+
+    static void welcome(String name) {
         System.out.printf("Welcome, %s!%n", name);
+    }
+
+    static int mult(int a, int b) {
+        return a * b;
     }
 }
