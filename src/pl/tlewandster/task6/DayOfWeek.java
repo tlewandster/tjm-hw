6605e2ce93bbc0ab31532e49b0beb56a5c8e2ac8
Tomasz Lewandowski <tlewandster@gmail.com>
2026-07-29 00:02:38 +0200

    feat: Add a method to handle sub-task 1-4

---
+++ src/pl/tlewandster/task6/DayOfWeek.java
@@ -0,0 +1,5 @@
+package pl.tlewandster.task6;
+
+public enum DayOfWeek {
+    MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY
+}
---
+++ src/pl/tlewandster/task6/Main.java
@@ -0,0 +1,36 @@
+package pl.tlewandster.task6;
+
+import static pl.tlewandster.task6.Utils.printSeparator;
+import static pl.tlewandster.task6.Utils.printTitle;
+
+@SuppressWarnings("JavaPrintToLogpoint")
+public class Main {
+    static void main() {
+        printTitle("1. Prosty enum – dni tygodnia");
+        DayOfWeek wednesday = DayOfWeek.WEDNESDAY;
+        System.out.println(wednesday);
+        printSeparator();
+
+        printTitle("2. Enum i pętla");
+        for (DayOfWeek dayOfWeek : DayOfWeek.values()) {
+            System.out.println(dayOfWeek);
+        }
+        printSeparator();
+
+        printTitle("3. Porównywanie enum");
+        DayOfWeek someDay1 = DayOfWeek.MONDAY;
+        DayOfWeek someDay2 = DayOfWeek.MONDAY;
+        System.out.println(someDay1 == someDay2);
+        printSeparator();
+
+        printTitle("4. Enum w switch");
+        String partOfWeek = switch (wednesday) {
+            case MONDAY, TUESDAY -> "The beginning of the week";
+            case WEDNESDAY, THURSDAY -> "Midweek";
+            case FRIDAY, SATURDAY, SUNDAY -> "Weekend";
+        };
+        System.out.println(partOfWeek);
+        printSeparator();
+    }
+}
+
