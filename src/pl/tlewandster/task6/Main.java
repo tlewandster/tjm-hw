56a5b663383554bb54ad40907f14b85230878a9a
Tomasz Lewandowski <tlewandster@gmail.com>
2026-07-29 09:58:50 +0200

    feat: Add a method to handle sub-task 6

--- src/pl/tlewandster/task6/Main.java
+++ src/pl/tlewandster/task6/Main.java
@@ -6,24 +6,24 @@ import static pl.tlewandster.task6.Utils.printTitle;
 @SuppressWarnings("JavaPrintToLogpoint")
 public class Main {
     static void main() {
-        printTitle("1. Prosty enum – dni tygodnia");
+        printTitle("1. A Simple Enum – Days of the Week");
         DayOfWeek wednesday = DayOfWeek.WEDNESDAY;
         System.out.println(wednesday);
         printSeparator();
 
-        printTitle("2. Enum i pętla");
+        printTitle("2. Enum and Loop");
         for (DayOfWeek dayOfWeek : DayOfWeek.values()) {
             System.out.println(dayOfWeek);
         }
         printSeparator();
 
-        printTitle("3. Porównywanie enum");
+        printTitle("3. Comparing Enums");
         DayOfWeek someDay1 = DayOfWeek.MONDAY;
         DayOfWeek someDay2 = DayOfWeek.MONDAY;
         System.out.println(someDay1 == someDay2);
         printSeparator();
 
-        printTitle("4. Enum w switch");
+        printTitle("4. Enum in a switch statement");
         String partOfWeek = switch (wednesday) {
             case MONDAY, TUESDAY -> "The beginning of the week";
             case WEDNESDAY, THURSDAY -> "Midweek";
@@ -32,9 +32,15 @@ public class Main {
         System.out.println("Wednesday: " + partOfWeek);
         printSeparator();
 
-        printTitle("5. Enum z metodą");
+        printTitle("5. Enum with a method");
         System.out.println("Wednesday: " + wednesday.isWeekend());
         System.out.println("Sunday: " + DayOfWeek.SUNDAY.isWeekend());
+        printSeparator();
+
+        printTitle("6. Enum with a field and a constructor");
+        for (Month month : Month.values()) {
+            System.out.printf("%s has %d days %n", month.name(),month.getDays());
+        }
         printSeparator();
     }
 }
