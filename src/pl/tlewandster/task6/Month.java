9f0d939701cb32cdf47f8b5d65e10f105c241f0b
Tomasz Lewandowski <tlewandster@gmail.com>
2026-07-29 22:03:06 +0200

    refactor: add overloaded Month constructor for explicit holiday season initialization

--- src/pl/tlewandster/task6/Month.java
+++ src/pl/tlewandster/task6/Month.java
@@ -1,16 +1,19 @@
 package pl.tlewandster.task6;
 
 public enum Month {
-    JANUARY(31), FEBRUARY(28), MARCH(31), APRIL(30), MAY(31), JUNE(30), JULY(31), AUGUST(31), SEPTEMBER(30), OCTOBER(31), NOVEMBER(30), DECEMBER(31);
+    JANUARY(31), FEBRUARY(28), MARCH(31), APRIL(30), MAY(31), JUNE(30, true), JULY(31, true), AUGUST(31,true), SEPTEMBER(30), OCTOBER(31), NOVEMBER(30), DECEMBER(31);
 
     private final int days;
-    private boolean isHolidaySeason = false;
+    private final boolean isHolidaySeason;
 
     Month(int days) {
         this.days = days;
-        if (this.name().equals("JULY") || this.name().equals("JUNE") || this.name().equals("AUGUST")) {
-            this.isHolidaySeason = true;
-        }
+        this.isHolidaySeason = false;
+    }
+
+    Month(int days, boolean isHolidaySeason){
+        this.days = days;
+        this.isHolidaySeason = isHolidaySeason;
     }
 
     public boolean isHolidaySeason() {
