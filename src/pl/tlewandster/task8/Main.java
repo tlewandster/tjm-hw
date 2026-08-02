25dffd8c539eeec32b2d9ae1243e525a4e46d955
Tomasz Lewandowski <tlewandster@gmail.com>
2026-08-02 21:15:58 +0200

    feat(task-8):add sample flight execution in Main

--- src/pl/tlewandster/task8/Main.java
+++ src/pl/tlewandster/task8/Main.java
@@ -1,9 +1,16 @@
 package pl.tlewandster.task8;
 
+import java.util.List;
+
 public class Main {
 
     static void main() {
-
+        List<Flight> flights = List.of(
+                new Flight("001", "2025-01-15", "10:00:00", "LCJ", "2025-01-15", "12:30:00", "WAW")
+                );
+        for (Flight flight : flights) {
+            flight.calculateFlightDuration();
+        }
     }
 }
 
