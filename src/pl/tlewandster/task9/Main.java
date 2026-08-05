deaf4bba9c4913ab4d6cfebcee4090df3a9def84
Tomasz Lewandowski <tlewandster@gmail.com>
2026-08-06 00:41:35 +0200

    feat: Add a method to handle sub-task 1

---
+++ src/pl/tlewandster/task9/Main.java
@@ -0,0 +1,9 @@
+package pl.tlewandster.task9;
+
+public class Main {
+    static void main() {
+
+        // Zadanie 1: Walidator hasła (unchecked)
+        PasswordValidator.validate("password1");
+    }
+}
