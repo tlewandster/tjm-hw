58053245697c1f303ecf0bdd8f047a0bd02c3a06
Tomasz Lewandowski <tlewandster@gmail.com>
2026-08-22 13:56:37 +0200

    feat: Refactor test cases in Test class for improved readability

--- src/pl/tlewandster/task10/Test.java
+++ src/pl/tlewandster/task10/Test.java
@@ -7,20 +7,26 @@ public class Test {
 
     static void test_1(){
         System.out.println("Test 1.1");
-        Pair<Integer, String> p1 = Pair.of(1, "a");
-        Pair<Integer, String> p2 = Pair.of(1, "a");
-        Pair<Integer, String> p3 = Pair.of(1, "b");
-        boolean p1EqualsP2 = p1.equals(p2);
-        boolean p1EqualsP3 = p1.equals(p3);
-        boolean p1HashcodetoP2Hashcode = p1.hashCode() == p2.hashCode();
-        System.out.println("p1EqualsP2 = " + p1EqualsP2);
-        System.out.println("p1EqualsP3 = " + p1EqualsP3);
-        System.out.println("p1HashcodetoP2Hashcode = " + p1HashcodetoP2Hashcode);
+        Pair<Integer, String> testPair1 = Pair.of(1, "a");
+        Pair<Integer, String> testPair2 = Pair.of(1, "a");
+        Pair<Integer, String> testPair3 = Pair.of(1, "b");
+        boolean testPair1EqualstestPair2 = testPair1.equals(testPair2);
+        boolean testPair1EqualstestPair3 = testPair1.equals(testPair3);
+        boolean testPair1HashcodetotestPair2Hashcode = testPair1.hashCode() == testPair2.hashCode();
+        System.out.println("testPair1EqualstestPair2 = " + testPair1EqualstestPair2);
+        System.out.println("testPair1EqualstestPair3 = " + testPair1EqualstestPair3);
+        System.out.println("testPair1HashcodetotestPair2Hashcode = " + testPair1HashcodetotestPair2Hashcode);
         System.out.println();
 
         System.out.println("Test 1.2");
-        Pair<Integer, String> p4 = Pair.of(null, "x");
-        System.out.println("p4.first() = " + p4.first());
-        System.out.println(p4);
+        Pair<Integer, String> testPair4 = Pair.of(null, "x");
+        System.out.println("testPair4.first() = " + testPair4.first());
+        System.out.println(testPair4);
+        System.out.println();
+
+        System.out.println("Test 1.3");
+        Pair<String, String> testPair5 = Pair.of("A", "B");
+//        testPair5.first = "B";
+        System.out.println("java: first has private access in pl.tlewandster.task10.Pair");
     }
 }
