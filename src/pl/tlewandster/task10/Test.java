83c9625067c6b837b741c920b277405d61f485ca
Tomasz Lewandowski <tlewandster@gmail.com>
2026-08-22 13:27:31 +0200

    feat: Add Test class with initial test cases for Pair functionality

---
+++ src/pl/tlewandster/task10/Test.java
@@ -0,0 +1,20 @@
+package pl.tlewandster.task10;
+
+public class Test {
+    static void main() {
+        test_1();
+    }
+
+    static void test_1(){
+        System.out.println("Test 1.1");
+        Pair<Integer, String> p1 = Pair.of(1, "a");
+        Pair<Integer, String> p2 = Pair.of(1, "a");
+        Pair<Integer, String> p3 = Pair.of(1, "b");
+        boolean p1EqualsP2 = p1.equals(p2);
+        boolean p1EqualsP3 = p1.equals(p3);
+        boolean p1HashcodetoP2Hashcode = p1.hashCode() == p2.hashCode();
+        System.out.println("p1EqualsP2 = " + p1EqualsP2);
+        System.out.println("p1EqualsP3 = " + p1EqualsP3);
+        System.out.println("p1HashcodetoP2Hashcode = " + p1HashcodetoP2Hashcode);
+    }
+}
