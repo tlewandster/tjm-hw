22f3232f2696880bc88a2c90c57990baae0c4218
Tomasz Lewandowski <tlewandster@gmail.com>
2026-08-22 15:02:03 +0200

    feat: Add isOutOfBounds method and enhance swap method with null and bounds checks

--- src/pl/tlewandster/task10/ArrayUtils.java
+++ src/pl/tlewandster/task10/ArrayUtils.java
@@ -4,7 +4,16 @@ public final class ArrayUtils {
     private ArrayUtils() {
     }
 
-    public static <T> void swap(T[] arr, int i, int j) {
+    public static <T> boolean isOutOfBounds(T[] arr, int i) {
+        return i < 0 || i >= arr.length;
+    }
 
+    public static <T> void swap(T[] arr, int i, int j) {
+        if (arr == null) {
+            throw new NullPointerException();
+        }
+        if (isOutOfBounds(arr,i)||isOutOfBounds(arr,j)){
+            throw new IndexOutOfBoundsException();
+        };
     }
 }
