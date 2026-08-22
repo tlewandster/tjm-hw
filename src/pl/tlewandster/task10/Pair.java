0c920de083a93e40fd9c1577437e25d64d97e9d4
Tomasz Lewandowski <tlewandster@gmail.com>
2026-08-22 13:27:23 +0200

    feat: Enhance Pair class with constructor and method implementations

--- src/pl/tlewandster/task10/Pair.java
+++ src/pl/tlewandster/task10/Pair.java
@@ -4,14 +4,40 @@ public final class Pair<T, U> {
     private final T first;
     private final U second;
 
-    private Pair(T first, U second) { /* ... */ }
+    private Pair(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
 
-    public static <T, U> Pair<T, U> of(T first, U second) { /* ... */ }
+    public static <T, U> Pair<T, U> of(T first, U second) {
+        return new Pair<>(first, second);
+    }
 
-    public T first() { /* ... */ }
-    public U second() { /* ... */ }
+    public T first() {
+        return first;
+    }
 
-    @Override public boolean equals(Object o) { /* zgodne z kontraktem */ }
-    @Override public int hashCode() { /* zgodne z equals */ }
-    @Override public String toString() { /* np. "Pair[first=..., second=...]" */ }
+    public U second() {
+        return second;
+    }
+
+    @Override
+    public String toString() {
+        return "Pair{" + "first=" + first + ", second=" + second + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Pair<?, ?> pair)) return false;
+
+        return first.equals(pair.first) && second.equals(pair.second);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = first.hashCode();
+        result = 31 * result + second.hashCode();
+        return result;
+    }
+
 }
