ed4c71c4c24ba16e15292ad61c4bd891cc18f139
Tomasz Lewandowski <tlewandster@gmail.com>
2026-09-03 22:17:36 +0200

    feat: Add Exercise_4 class for basic arithmetic operations using functional interfaces

--- src/pl/tlewandster/task12/Exercise_3.java
+++ src/pl/tlewandster/task12/Exercise_3.java
@@ -6,7 +6,7 @@ import java.util.function.Function;
 public class Exercise_3 {
     static void main() {
         Function<String, Integer> lengthWithoutSpaces = s -> s.replace(" ", "").length();
-        Function<String,Integer> numberVowels = s-> s.replaceAll("(?i)[^aeiouy]", "").length();
+        Function<String, Integer> numberVowels = s -> s.replaceAll("(?i)[^aeiouy]", "").length();
         System.out.println("Enter sentence:");
         String string = new Scanner(System.in).nextLine();
         System.out.println("Length without spaces: " + lengthWithoutSpaces.apply(string));
